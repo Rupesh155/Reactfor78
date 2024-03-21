2ac83bfa97059215285360be6ec145167cfe1c96
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-03-21 18:12:20 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -21,14 +21,55 @@
 
 
 
+// import React from 'react'
+// import Home from './Home'
+// import CounterWithLogin from './Couter'
+// import Clock from './Clock'
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <Home/> */}
+//       {/* <CounterWithLogin/> */}
+//       <Home/>
+//       {/* <Clock/> */}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
 import React from 'react'
+import Navbar from './Navbar'
+import './App.css'
+import {Route,Routes}  from 'react-router-dom'
 import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <>
+    
+      <Navbar/>
+
+
+      <Routes>
+
+        <Route   path='/' element={<Home/>}  />
+        <Route   path='/about' element={<About/>}  />
+        <Route   path='/contact' element={<Contact/>}  />
+
+
+      </Routes>
+
+
+    
+    </>
   )
 }
 
